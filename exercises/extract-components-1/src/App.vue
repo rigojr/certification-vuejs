9d76c659c5b8f399419856a546bd40d2b77c1c3e
José Rigoberto Salas Chacón <rigo@tinyurl.com>
2025-02-25 16:15:25 -0500

    complet challenge

--- exercises/extract-components-1/src/App.vue
+++ exercises/extract-components-1/src/App.vue
@@ -3,19 +3,37 @@ import { computed, reactive, ref } from "vue";
 /*
 These are Icons that you can use, of course you can use other ones if you prefer.
 */
-import { StarIcon, TrashIcon, PencilIcon } from "@heroicons/vue/24/solid";
 import { items } from "./movies.json";
+import MovieItem from "./MovieItem.vue";
 
 const movies = ref(items);
 
-function updateRating(movieIndex, rating) {
-  movies.value[movieIndex].rating = rating;
+function updateRating(id, rating) {
+  console.log(id, rating);
+  const movie = movies.value.find((movie) => movie.id === id);
+
+  if (movie === undefined) {
+    return;
+  }
+
+  movie.rating = rating;
 }
-function removeMovie(movieIndex) {
+
+function removeMovie(id) {
+  const movieIndex = movies.value.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return;
+  }
+
   movies.value = movies.value.filter((movie, index) => index !== movieIndex);
 }
-function editMovie(movieIndex) {
-  const movie = movies.value[movieIndex];
+function editMovie(id) {
+  const movie = movies.value.find((movie) => movie.id === id);
+
+  if (movie === undefined) {
+    return;
+  }
 
   form.id = movie.id;
   form.name = movie.name;
@@ -283,85 +301,14 @@ function removeRatings() {
       </div>
     </div>
     <div class="movie-list">
-      <div
-        class="movie-item group"
-        v-for="(movie, movieIndex) in movies"
+      <MovieItem
+        v-for="movie in movies"
         :key="movie.id"
-      >
-        <div class="movie-item-image-wrapper">
-          <div class="movie-item-star-wrapper">
-            <StarIcon
-              id="rating"
-              class="movie-item-star-rating-icon"
-              :class="[movie.rating ? 'text-yellow-500' : 'text-gray-500']"
-            />
-            <div class="movie-item-star-content-wrapper">
-              <span
-                v-if="movie.rating"
-                id="rating-stars"
-                class="movie-item-star-content-rating-rated"
-              >
-                {{ movie.rating }}
-              </span>
-              <span v-else class="movie-item-star-content-rating-not-rated">
-                -
-              </span>
-            </div>
-          </div>
-          <img :src="movie.image" class="movie-item-image" alt="" />
-        </div>
-
-        <div class="movie-item-content-wrapper">
-          <div class="movie-item-title-wrapper">
-            <h3 class="movie-item-title">{{ movie.name }}</h3>
-            <div class="movie-item-genres-wrapper">
-              <span
-                v-for="genre in movie.genres"
-                :key="`${movie.id}-${genre}`"
-                class="movie-item-genre-tag"
-                >{{ genre }}</span
-              >
-            </div>
-          </div>
-          <div class="movie-item-description-wrapper">
-            <p class="movie-item-description">{{ movie.description }}</p>
-          </div>
-          <div class="movie-item-rating-wrapper">
-            <span class="movie-item-rating-text">
-              Rating: ({{ movie.rating }}/5)
-            </span>
-            <div class="movie-item-star-icon-wrapper">
-              <button
-                v-for="star in 5"
-                :key="star"
-                class="movie-item-star-icon-button"
-                :class="[
-                  star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
-                ]"
-                :disabled="star === movie.rating"
-                @click="updateRating(movieIndex, star)"
-              >
-                <StarIcon class="movie-item-star-icon" />
-              </button>
-            </div>
-
-            <div class="movie-item-actions-list-wrapper">
-              <button
-                class="movie-item-action-edit-button"
-                @click="editMovie(movieIndex)"
-              >
-                <PencilIcon class="w-4 h-4" />
-              </button>
-              <button
-                class="movie-item-action-remove-button"
-                @click="removeMovie(movieIndex)"
-              >
-                <TrashIcon class="w-4 h-4" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+        :movie="movie"
+        @edit="editMovie"
+        @remove="removeMovie"
+        @update:rating="updateRating"
+      />
     </div>
   </div>
 </template>
--- exercises/extract-components-1/src/MovieItem.vue
+++ exercises/extract-components-1/src/MovieItem.vue
@@ -1,7 +1,89 @@
 <script setup>
-// Start your logic here
+import { StarIcon, TrashIcon, PencilIcon } from "@heroicons/vue/24/solid";
+import { computed } from "vue";
+
+const props = defineProps(["movie"]);
+const emits = defineEmits(["edit", "remove", "update:rating"])
+
+const notRated = computed(
+  () => props.movie.rating === undefined || props.movie.rating <= 0,
+);
 </script>
 
 <template>
-  <!-- Start your template here -->
+  <div class="movie-item group">
+    <div class="movie-item-image-wrapper">
+      <div class="movie-item-star-wrapper">
+        <StarIcon
+          id="rating"
+          class="movie-item-star-rating-icon"
+          :class="[!notRated ? 'text-yellow-500' : 'text-gray-500']"
+        />
+        <div class="movie-item-star-content-wrapper">
+          <span
+            v-if="!notRated"
+            id="rating-stars"
+            class="movie-item-star-content-rating-rated"
+          >
+            {{ movie.rating }}
+          </span>
+          <span v-else class="movie-item-star-content-rating-not-rated">
+            -
+          </span>
+        </div>
+      </div>
+      <img :src="movie.image" class="movie-item-image" alt="" />
+    </div>
+
+    <div class="movie-item-content-wrapper">
+      <div class="movie-item-title-wrapper">
+        <h3 class="movie-item-title">{{ movie.name }}</h3>
+        <div class="movie-item-genres-wrapper">
+          <span
+            v-for="genre in movie.genres"
+            :key="`${movie.id}-${genre}`"
+            class="movie-item-genre-tag"
+            >{{ genre }}</span
+          >
+        </div>
+      </div>
+      <div class="movie-item-description-wrapper">
+        <p class="movie-item-description">{{ movie.description }}</p>
+      </div>
+      <div class="movie-item-rating-wrapper">
+        <span class="movie-item-rating-text">
+          Rating: ({{ movie.rating }}/5)
+        </span>
+        <div class="movie-item-star-icon-wrapper">
+          <button
+            v-for="star in 5"
+            :key="star"
+            class="movie-item-star-icon-button"
+            :class="[
+              star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
+            ]"
+            :disabled="star === movie.rating"
+            @click="emits('update:rating', movie.id, star)"
+          >
+            <StarIcon class="movie-item-star-icon" />
+          </button>
+        </div>
+
+        <div class="movie-item-actions-list-wrapper">
+          <button
+            class="movie-item-action-edit-button"
+            @click="emits('edit', movie.id)"
+          >
+            <PencilIcon class="w-4 h-4" />
+          </button>
+          <button
+            class="movie-item-action-remove-button"
+            @click="emits('remove', movie.id)"
+          >
+            <TrashIcon class="w-4 h-4" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
